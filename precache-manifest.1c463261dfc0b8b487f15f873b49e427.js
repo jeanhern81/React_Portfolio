628cf438668bac9f28173a4908adc0c2925754df
jeanhern81 <jeanhern81@gmail.com>
2020-09-29 21:31:37 -0700

    Updates

---
+++ precache-manifest.1c463261dfc0b8b487f15f873b49e427.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b03406f4edd197dbaa5e03b1a259e2",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "ec8f89d24faa00a0274d",
+    "url": "/React_Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2f215adcb8ea76b7cc1d",
+    "url": "/React_Portfolio/static/css/main.fa4bb2c7.chunk.css"
+  },
+  {
+    "revision": "ec8f89d24faa00a0274d",
+    "url": "/React_Portfolio/static/js/2.63ffa27b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Portfolio/static/js/2.63ffa27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f215adcb8ea76b7cc1d",
+    "url": "/React_Portfolio/static/js/main.35230e69.chunk.js"
+  },
+  {
+    "revision": "6f47035444c6a860ca23",
+    "url": "/React_Portfolio/static/js/runtime-main.6c723fb5.js"
+  },
+  {
+    "revision": "21c13f7fd5159c189ec1cc69c3ad81de",
+    "url": "/React_Portfolio/static/media/alexandria.21c13f7f.jpg"
+  },
+  {
+    "revision": "e96778e87874cb55def462e1083e6b0c",
+    "url": "/React_Portfolio/static/media/burger.e96778e8.jpg"
+  },
+  {
+    "revision": "f7f75d44dc3691a0f87c33b976a2c046",
+    "url": "/React_Portfolio/static/media/codeQuiz.f7f75d44.jpg"
+  },
+  {
+    "revision": "4c57e9fac7d512f4ae74b04d56aab85e",
+    "url": "/React_Portfolio/static/media/jeanette.4c57e9fa.png"
+  },
+  {
+    "revision": "2236291643b22024e8a877c5a0eed0c5",
+    "url": "/React_Portfolio/static/media/jeanettehernandez.22362916.png"
+  },
+  {
+    "revision": "c7d01f63f0205535e5d4c01f19870b45",
+    "url": "/React_Portfolio/static/media/passwordGen.c7d01f63.jpg"
+  },
+  {
+    "revision": "aa2a2e3ac15ebe985db90b733f3feff7",
+    "url": "/React_Portfolio/static/media/scheduler.aa2a2e3a.jpg"
+  },
+  {
+    "revision": "e156237e1f06d27ce8fb243690c4e84a",
+    "url": "/React_Portfolio/static/media/watercolor-background.e156237e.jpg"
+  },
+  {
+    "revision": "60a0c1b9800dff09f2d834f5fc750527",
+    "url": "/React_Portfolio/static/media/weatherDash.60a0c1b9.jpg"
+  }
+]);
