f1bc7952ade7a06134ea6d5b87dceab9b7b480ea
jeanhern81 <jeanhern81@gmail.com>
2021-02-22 16:18:07 -0800

    added julio's to portfolio

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,12 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from '../components/Card'
 
-
+import jGarciaIns from '../assets/images/projects/jGarciaIns.JPG'
 import burger from '../assets/images/projects/burger.jpg';
 import scheduler from '../assets/images/projects/scheduler.jpg';
 import weatherDash from '../assets/images/projects/weatherDash.jpg';
 import codeQuiz from '../assets/images/projects/codeQuiz.jpg';
-import passwordGen from '../assets/images/projects/passwordGen.jpg';
 import alexandria from '../assets/images/projects/alexandria.jpg';
 
 
@@ -19,8 +18,19 @@ class Carousel extends React.Component {
         super(props);
         this.state = {
             items:[
+
                 {
                     id: 0,
+                    title: 'Julio Garcia Insurance Agency',
+                    subTitle: 'Over 20 years of experience',
+                    text: 'Technologies Used: HTML, CSS, Bootstrap, JavaScript,',
+                    imgSrc: jGarciaIns,
+                    link: 'https://jgarciains.com/',
+                    link2: 'https://github.com/jeanhern81/JulioGarciaInsurance.git',
+                    selected: false
+                },
+                {
+                    id: 1,
                     title: 'Eat-Da-Burger',
                     subTitle: 'Enjoy a delicious burger',
                     text: 'Technologies Used: HTML, CSS, Bootstrap, JavaScript, jQuery, Node Js, Express JS, MySql, Heroku',
@@ -30,7 +40,7 @@ class Carousel extends React.Component {
                     selected: false
                 },
                 {
-                    id: 1, 
+                    id: 2, 
                     title: 'Work Day Scheduler',
                     subTitle: 'Hourly task schedule',
                     imgSrc: scheduler,
@@ -39,7 +49,7 @@ class Carousel extends React.Component {
                     selected: false
                 },
                 {
-                    id: 2, 
+                    id: 3, 
                     title: 'Weather Dashboard',
                     subTitle: 'Up to date weather report',
                     imgSrc: weatherDash,
@@ -48,7 +58,7 @@ class Carousel extends React.Component {
                     selected: false
                 },
                 {
-                    id: 3,
+                    id: 4,
                     title: 'Code Quiz',
                     subTitle: 'Test your coding skills',
                     imgSrc: codeQuiz,
@@ -56,15 +66,7 @@ class Carousel extends React.Component {
                     link2: 'https://github.com/jeanhern81/code-quiz',
                     selected: false
                 },
-                {
-                    id: 4,
-                    title: 'Password Generator',
-                    subTitle: 'Easy to use random password generator',
-                    imgSrc: passwordGen,
-                    link: 'https://jeanhern81.github.io/javascript-password-generator/',
-                    link2: 'https://github.com/jeanhern81/javascript-password-generator',
-                    selected: false
-                },
+                
                 {
                     id: 5,
                     title: 'Alexandria',
